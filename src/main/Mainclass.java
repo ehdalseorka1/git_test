d380aea0cfd5653a12b6d355b7e52d13a5e29160
BDM <ehdalseorka2@gmail.com>
2021-03-02 20:07:41 +0900

    기본프로젝트 생성

--- src/main/Mainclass.java
+++ src/main/Mainclass.java
@@ -2,6 +2,8 @@ package main;
 
 import java.util.Scanner;
 
+import login.Login;
+
 public class Mainclass {
 	public static void main(String[] args) {
 		
@@ -16,6 +18,8 @@ public class Mainclass {
 			num = input.nextInt();
 			switch(num) {
 			case 1:
+				Login lo = new Login();
+				lo.login();
 				break;
 			case 2:
 				break;
